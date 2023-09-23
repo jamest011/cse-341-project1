19d5166f900346cf52a2aa056594446237d7fd47
Jamest011 <ttysjr@gmail.com>
2023-09-23 14:01:24 -0500

    rest api finish and contacts update

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -11,7 +11,7 @@ const getAll = async (req,res) => {
 
 const getSingle = async (req,res) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: userId});
+    const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: userId });
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
@@ -27,7 +27,7 @@ const createContact = async (req,res) => {
         birthday: req.body.birthday
     };
     const response = await mongodb.getDatabase().db().collection('contacts').insertOne(user);
-    if (response.modifiedCount > 0) {
+    if (response.acknowledged) {
         res.status(204).send();
     } else {
         res.status(500).json(response.error || 'Some error occurred while updating the user.');
@@ -37,10 +37,11 @@ const createContact = async (req,res) => {
 const updateContact = async (req,res) => {
     const userId = new ObjectId(req.params.id);
     const user = {
-        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
         email: req.body.email,
-        name: req.body.name,
-        ipaddress: req.body.ipaddress
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
     };
     const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: userId }, user);
     if (response.modifiedCount > 0) {
@@ -52,11 +53,11 @@ const updateContact = async (req,res) => {
 
 const deleteContact = async (req,res) => {
     const userId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({_id: userId });
+    const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: userId });
     if (response.deletedCount > 0) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the user.');
+        res.status(500).json(response.error || 'Some error occurred while deleting the user.');
     }
 };
 
